58a2ff1e7473e8df21d7f07fc88ce478e19a772f
codup <wowas18@nm.ru>
2016-10-18 18:26:35 +0400

    port to Odoo 10.0

--- cron/wizard/start_cron.py
+++ cron/wizard/start_cron.py
@@ -1,32 +1,20 @@
 ﻿# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2014 CodUP (<http://codup.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    Odoo
+#    Copyright (C) 2014-2016 CodUP (<http://codup.com>).
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
-from openerp.service.server import ThreadedServer
+from odoo.service.server import ThreadedServer
+from odoo import api, fields, models
 
-class cron_start_cron(osv.osv_memory):
+
+class cron_start_cron(models.TransientModel):
     _name = 'cron.start.cron'
     _description = 'Start cron'
 
-    def start_cron(self, cr, uid, ids, context=None):
+    def start_cron(self):
         ThreadedServer(None).cron_spawn()
         return {'type': 'ir.actions.act_window_close',}
 
